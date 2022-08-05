cb310228d480fbb7c8f8114957b332a2d34c530b
sarin.jickson@accubits.com <sarin.jickson@accubits.com>
2022-08-05 21:31:17 +0530

    Posting with postman

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const xpress = require('express');
 const app = xpress();
+const bodyParser = require('body-parser');
 
+// Handling the parsing
+app.use(bodyParser.json());
 
   app.post('/',function(req,res){
   var email = req.body.email;
