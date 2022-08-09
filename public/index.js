0c5ef12a6c72999029e49ba2bdb6e588df607aa3
sarin.jickson@accubits.com <sarin.jickson@accubits.com>
2022-08-09 14:16:20 +0530

    Getting back information

--- public/index.js
+++ public/index.js
@@ -7,6 +7,10 @@ constructor(props){
     'email'  : "",
   }
 }
+async componentDidMount(){
+  const result = await axios.get('/get_total_amount');
+  this.setState({total_amount :  result.data["0"].total_amount});
+}
 onSubmit = async (event) =>{
   event.preventDefault();
   const response = await axios.post('/post_info',{
