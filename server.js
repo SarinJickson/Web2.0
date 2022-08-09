c3332ba28b0494f8a5260781d640693b33450d6d
sarin.jickson@accubits.com <sarin.jickson@accubits.com>
2022-08-09 16:43:33 +0530

    install paypal-sdk

--- server.js
+++ server.js
@@ -4,9 +4,18 @@ const bodyParser = require('body-parser');
 const {save_user_information} = require('./models/server_db');
 const path = require('path');
 const publicPath = path.join(__dirname, './public');
+const paypal = require('paypal-rest-sdk');
 // Handling the parsing
 app.use(bodyParser.json());
 app.use(xpress.static(publicPath));
+
+// paypal configuration
+paypal.configure({
+  'mode': 'sandbox', //sandbox or live
+  'client_id': 'AdCEjhwcOAQD5R7c_753RoFBfhnypm1KH1NB7CptCn_UzKVH9KVcfQWBYJ9NGv_Z8RMUFxbNjoYy-L8m',
+  'client_secret': 'EDKCLSK4O5QDn4T48TdO13XJ_rFSoZLoFnPT4hF1OBl67Ihdps51kZd_rNhB_3VDLkOmwDVPy70ITx5R'
+});
+
   app.post('/post_info', async (req,res) => {
   var email = req.body.email;
   var amount = req.body.amount;
